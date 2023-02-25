5a69c5a093e51a2fee9fff97f63622a507599e51
Ayush Pathak <126178709+ActLikeAyush@users.noreply.github.com>
2023-02-25 22:18:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ fetch(' https://hindi-jokes-api.onrender.com/jokes?api_key=463779afaf2f3ec341b96
   const jokeText= jokeData.jokeContent;
   const jokeName= jokeData.created_by
   const joke = document.getElementById('joke');
-  const joke1= document.getElementById('joke1');
   joke.innerHTML = jokeText;
-  joke1.innerHTML=jokeName;
+ 
 })
